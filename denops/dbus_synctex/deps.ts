e470f28ffad4cfead58ef5a4aa9cdd2ca1b5d4aa
kamecha <masakikameyama81@gmail.com>
2023-11-12 23:16:39 +0900

    denopsでAPI登録の雛形作成

---
+++ denops/dbus_synctex/deps.ts
@@ -0,0 +1,1 @@
+export type { Denops } from "https://deno.land/x/denops_std@v5.0.2/mod.ts";
---
+++ denops/dbus_synctex/main.ts
@@ -0,0 +1,6 @@
+import type { Denops } from "./deps.ts";
+
+export async function main(denops: Denops) {
+  denops.dispatcher = {
+  };
+}
