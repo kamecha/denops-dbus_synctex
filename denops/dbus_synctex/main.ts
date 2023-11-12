b02937a30445204815b2ab3ab9839912f6d3ecc1
kamecha <masakikameyama81@gmail.com>
2023-11-12 23:25:43 +0900

    syncView APIの追加

--- denops/dbus_synctex/main.ts
+++ denops/dbus_synctex/main.ts
@@ -10,5 +10,36 @@ export async function main(denops: Denops) {
   );
 
   denops.dispatcher = {
+    async syncView(
+      texPath: unknown,
+      pdfPath: unknown,
+      line: unknown,
+      column: unknown,
+    ): Promise<void> {
+      const daemon = obj.getInterface("org.gnome.evince.Daemon");
+      const pdfURI = `file://${pdfPath}`;
+      const owner: string = await daemon.FindDocument(
+        pdfURI,
+        false,
+      );
+      const evince = await bus.getProxyObject(
+        owner,
+        "/org/gnome/evince/Evince",
+      );
+      const app = evince.getInterface("org.gnome.evince.Application");
+      const windowList = await app.GetWindowList();
+      if (windowList.length === 0) {
+        throw new Error("No window found");
+      }
+
+      const windowPath = windowList[0];
+      const windowProxy = await bus.getProxyObject(owner, windowPath);
+      const window = windowProxy.getInterface("org.gnome.evince.Window");
+      await window.SyncView(
+        texPath,
+        [line, column],
+        0,
+      );
+    },
   };
 }
