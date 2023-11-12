aecc3ec2115ab792cb7ebbaca5cc8c589c9a0f68
kamecha <masakikameyama81@gmail.com>
2023-11-12 23:29:15 +0900

    syncSource用のcallback APIを追加した。

--- denops/dbus_synctex/main.ts
+++ denops/dbus_synctex/main.ts
@@ -41,5 +41,42 @@ export async function main(denops: Denops) {
         0,
       );
     },
+    async registerSyncSource(
+      pdfPath: unknown,
+      callback: unknown,
+    ): Promise<void> {
+      const daemon = obj.getInterface("org.gnome.evince.Daemon");
+      const pdfURI = `file://${pdfPath}`;
+      const owner: string = await daemon.FindDocument(
+        pdfURI,
+        false,
+      );
+      const evince = await bus.getProxyObject(
+        owner,
+        "/org/gnome/evince/Evince",
+      );
+      const app = evince.getInterface("org.gnome.evince.Application");
+      const windowList = await app.GetWindowList();
+      if (windowList.length === 0) {
+        throw new Error("No window found");
+      }
+
+      const windowPath = windowList[0];
+      const windowProxy = await bus.getProxyObject(owner, windowPath);
+      const window = windowProxy.getInterface("org.gnome.evince.Window");
+      window.on(
+        "SyncSource",
+        async (path: string, point: [number, number], time: number) => {
+          await denops.call(
+            "denops#callback#call",
+            callback,
+            path,
+            point[0],
+            point[1],
+            time,
+          );
+        },
+      );
+    },
   };
 }
