9762e5b070ee26401f7da30ed06aa53194b65b3f
kamecha <masakikameyama81@gmail.com>
2023-11-12 23:24:57 +0900

    dbus daemonの取得

--- denops/dbus_synctex/main.ts
+++ denops/dbus_synctex/main.ts
@@ -4,6 +4,10 @@ import type { Denops } from "./deps.ts";
 export async function main(denops: Denops) {
   const bus = dbus.sessionBus();
   await bus.requestName("denops.dbus", 0);
+  const obj = await bus.getProxyObject(
+    "org.gnome.evince.Daemon",
+    "/org/gnome/evince/Daemon",
+  );
 
   denops.dispatcher = {
   };
