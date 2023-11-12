c1bb251980d93a05a9cfdf1c5abfe46957fa76dd
kamecha <masakikameyama81@gmail.com>
2023-11-12 23:18:48 +0900

    npmのdbusライブラリ使って、busの作成

--- denops/dbus_synctex/deps.ts
+++ denops/dbus_synctex/deps.ts
@@ -1,1 +1,2 @@
+export * as dbus from "npm:dbus-next@0.10.2";
 export type { Denops } from "https://deno.land/x/denops_std@v5.0.2/mod.ts";
--- denops/dbus_synctex/main.ts
+++ denops/dbus_synctex/main.ts
@@ -1,6 +1,10 @@
+import { dbus } from "./deps.ts";
 import type { Denops } from "./deps.ts";
 
 export async function main(denops: Denops) {
+  const bus = dbus.sessionBus();
+  await bus.requestName("denops.dbus", 0);
+
   denops.dispatcher = {
   };
 }
