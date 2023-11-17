f3c7a8d9b4ca78617cccd17785aff3372f23c7ae
kamecha <masakikameyama81@gmail.com>
2023-11-17 16:08:22 +0900

    busの作成とcallback登録のlua窓口の作成

--- lua/dbus_synctex/init.lua
+++ lua/dbus_synctex/init.lua
@@ -14,4 +14,30 @@ function M.getPdfPathCWD()
 	return M.getPdfPath(vim.fn.getcwd())
 end
 
+--- create Session Bus for DBus
+-- @return nil
+function M.createSessionBus()
+	vim.fn["dbus_synctex#createSessionBus"]()
+end
+
+--- destroy Session Bus for DBus
+-- @return nil
+function M.destroySessionBus()
+	vim.fn["dbus_synctex#destroySessionBus"]()
+end
+
+--- register callback to denops
+-- @param func function
+-- @return nil
+function M.registerCallback(func)
+	vim.fn["dbus_synctex#registerCallback"](func)
+end
+
+--- register callback which registerd to denops to dbus
+-- @param pdfPath string
+-- @return nil
+function M.registerSyncSource(pdfPath)
+	vim.fn["dbus_synctex#registerSyncSource"](pdfPath)
+end
+
 return M
