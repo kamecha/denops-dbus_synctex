ec06d304e67403fcbb1c1d9a71b97e9a72946f6e
kamecha <masakikameyama81@gmail.com>
2023-11-17 15:09:48 +0900

    pdfのPathの取得関数をluaでも登録

--- lua/dbus_synctex/init.lua
+++ lua/dbus_synctex/init.lua
@@ -1,4 +1,17 @@
 
 local M = {}
 
+--- get pdf Path under specified directory
+-- @param dir string
+-- @return string
+function M.getPdfPath(dir)
+	return vim.fn["dbus_synctex#getPdfPath"](dir)
+end
+
+--- get pdf Path under current directory
+-- @return string
+function M.getPdfPathCurrent()
+	return M.getPdfPath(vim.fn.getcwd())
+end
+
 return M
